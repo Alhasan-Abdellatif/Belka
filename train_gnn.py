ae03245e3863530fc47fc916dcc1a24bc5698ebf
Alhasan-Abdellatif <alhasanabdellatif@gmail.com>
2024-05-15 16:52:50 +0100

    valid

--- train_gnn.py
+++ train_gnn.py
@@ -12,10 +12,11 @@ import torch.nn as nn
 import torch.optim as optim
 from torch_geometric.nn import MessagePassing, global_mean_pool, global_max_pool
 from torch.nn import BCEWithLogitsLoss
-from sklearn.metrics import average_precision_score
+from sklearn.metrics import average_precision_score, roc_auc_score
 import matplotlib.pyplot as plt
 from absl import app, flags
-
+import time
+import datetime
 
 # Atom Featurisation
 ## Auxiliary function for one-hot enconding transformation based on list of
@@ -27,6 +28,7 @@ flags.DEFINE_string(name="test_csv_for_ids_path", default="../data/test.csv", he
 flags.DEFINE_string(name="test_csv_path", default="../shrunken_data/test.csv", help="")
 flags.DEFINE_string(name="train_file_csv_path", default="../shrunken_data/train.csv", help="")
 flags.DEFINE_string(name="test_file_path", default="../gnn_data/test.pt", help="")
+flags.DEFINE_string(name="valid_file_path", default="../gnn_data/valid.pt", help="")
 flags.DEFINE_string(name="results_dir", default="results", help="")
 flags.DEFINE_string(name="output_file_name", default="ids_pred_results.csv", help="")
 flags.DEFINE_string(name="saving_model_path", default="ids_pred_results.csv", help="")
@@ -99,6 +101,7 @@ def create_pytorch_geometric_graph_data_list_from_smiles_and_labels(x_smiles, y=
         
         # Node features
         atom_features = [get_atom_features(atom) for atom in mol.GetAtoms()]
+        #atom_features = np.stack( atom_features, axis=0 )
         x = torch.tensor(atom_features, dtype=torch.float)
         
         # Edge features
@@ -110,7 +113,9 @@ def create_pytorch_geometric_graph_data_list_from_smiles_and_labels(x_smiles, y=
             bond_feature = get_bond_features(bond)
             edge_features += [bond_feature, bond_feature]  # Same features in both directions
         
+        #edge_index = np.stack( edge_index, axis=0 )
         edge_index = torch.tensor(edge_index, dtype=torch.long).t().contiguous()
+        #edge_features = np.stack( edge_features, axis=0 )
         edge_attr = torch.tensor(edge_features, dtype=torch.float)
         
         # Creating the Data object
@@ -126,7 +131,7 @@ def create_pytorch_geometric_graph_data_list_from_smiles_and_labels(x_smiles, y=
 def featurize_data_in_batches(smiles_list, labels_list, batch_size):
     data_list = []
     # Define tqdm progress bar
-    #pbar = tqdm(total=len(smiles_list), desc="Featurizing data")
+    pbar = tqdm(total=len(smiles_list), desc="Featurizing data")
     for i in range(0, len(smiles_list), batch_size):
         smiles_batch = smiles_list[i:i+batch_size]
         if labels_list is not None:
@@ -136,9 +141,9 @@ def featurize_data_in_batches(smiles_list, labels_list, batch_size):
         #ids_batch = ids_list[i:i+batch_size]
         batch_data_list = create_pytorch_geometric_graph_data_list_from_smiles_and_labels(smiles_batch, labels_batch)
         data_list.extend(batch_data_list)
-        #pbar.update(len(smiles_batch))
+        pbar.update(len(smiles_batch))
         
-    #pbar.close()
+    pbar.close()
     return data_list
 
 
@@ -149,7 +154,8 @@ class CustomGNNLayer(MessagePassing):
 
     def forward(self, x, edge_index, edge_attr):
         # Start propagating messages
-        return MessagePassing.propagate(self, edge_index, x=x, edge_attr=edge_attr)
+        return MessagePassing.propagate(self, edge_index
+                                        , x=x, edge_attr=edge_attr)
 
     def message(self, x_j, edge_attr):
         combined = torch.cat((x_j, edge_attr), dim=1)
@@ -175,8 +181,7 @@ class GNNModel(nn.Module):
             x = self.bns[i](x)
             x = F.relu(x)
             x = self.dropout(x)
-
-
+            
         x = global_max_pool(x, data.batch) # Global pooling to get a graph-level representation
         x = self.lin(x)
         return x
@@ -184,13 +189,21 @@ class GNNModel(nn.Module):
 
 
 
-def train_model(df_train, num_epochs, input_dim, hidden_dim, num_layers, dropout_rate,out_channels, lr,
-                featurizing_batch_size=64,training_batch_size=32,shard_size=10000,shuffle = True,device = 'cuda:1'):
+def train_model(df_train, num_epochs,model, lr,results_dir,valid_loader,
+                featurizing_batch_size=64,training_batch_size=32,shard_size=10000,shuffle = True,device = 'cuda:1',es_patience = 3,):
+    
     # define model and loss
-    model = GNNModel(input_dim, hidden_dim, num_layers, dropout_rate,out_channels).to(device)
     optimizer = optim.AdamW(model.parameters(), lr=lr)
     criterion = BCEWithLogitsLoss()
     
+    start_time = time.time()
+
+    
+    best_val = None
+    patience = es_patience 
+    val_ap_list = []
+    val_roc_list = []
+
     # iteration over the training samples
     for epoch in range(num_epochs):
         model.train()
@@ -224,25 +237,74 @@ def train_model(df_train, num_epochs, input_dim, hidden_dim, num_layers, dropout
                 optimizer.step()
                 total_loss += loss.item()
  
+ 
+        # validation
+        val_predictions, labels= predict_with_model(model, valid_loader,device,is_labeled=True)
+        metrics = {
+                    "roc_auc_score": roc_auc_score(y_true=labels, y_score=val_predictions),
+                    "average_precision_score": average_precision_score(
+                        y_true=labels, y_score=val_predictions,average='micro'
+                    ),
+                }
+        
+        # During the first iteration (first epoch) best validation is set to None
+        val_ap = metrics["average_precision_score"]
+        val_roc = metrics["roc_auc_score"]
+        val_ap_list.append(val_ap)
+        val_roc_list.append(val_roc)
+        
+        #print(f'Epoch {epoch+1}/{num_epochs}, Loss: {total_loss / num_mini_batches}')
+        print('Epoch {:03}: | Loss: {:.5f} | Val Ap: {:.5f} | Val roc: {:.5f}  | Training time: {}'.format(
+            epoch + 1, 
+            total_loss/num_mini_batches, 
+            val_ap, 
+            val_roc,
+            str(datetime.timedelta(seconds=time.time() - start_time))[:7]))
 
-        print(f'Epoch {epoch+1}/{num_epochs}, Loss: {total_loss / num_mini_batches}')
+
+        if not best_val:
+            best_val = val_ap  # So any validation roc_auc we have is the best one for now
+            torch.save(model.state_dict(), os.path.join(results_dir,'best_val.pth'))  # Saving the model
+            #torch.save(model.state_dict(),
+            #               f"Fold{fold}_Epoch{epoch+1}_ValidAcc_{val_acc:.3f}_ROC_{val_roc:.3f}.pth")
+            continue
+            
+        if val_ap >= best_val:
+            best_val = val_ap
+            patience = es_patience  # Resetting patience since we have new best validation accuracy
+            torch.save(model.state_dict(), os.path.join(results_dir,'best_val.pth'))  # Saving the model 
+        else:
+            patience -= 1
+            if patience == 0:
+                print('Early stopping. Best Val roc_auc: {:.4f}'.format(best_val))
+                break  
+
+      
     
+    torch.save(val_ap_list, os.path.join(results_dir,'val_ap_list.pt'))  
+    torch.save(val_roc_list, os.path.join(results_dir,'val_roc_list.pt'))  
     return model
 
-def predict_with_model(model, test_loader,device):
+def predict_with_model(model, test_loader,device,is_labeled = False):
     model.eval()
     predictions = []
+    if is_labeled:
+        true_labels = []
     #molecule_ids = []
 
     with torch.no_grad():
         for data in tqdm(test_loader):
             output = torch.sigmoid(model(data.to(device)))
             predictions.extend(output.view(-1).tolist())
+            if is_labeled:
+                true_labels.extend(data.y.view(-1).tolist())
             #molecule_ids.extend(data.molecule_id)
+    if is_labeled:
+        return predictions,true_labels
+    else:
+        return predictions
 
-    return predictions
-
-def select_and_save_predictions_with_ids(predictions,test_df,path_test_file_for_ids,output_dir = 'results/',output_file_name ='ids_pred_results.csv'):
+def select_and_save_predictions_with_ids(predictions,test_df,path_test_file_for_ids,results_dir = 'results/',output_file_name ='ids_pred_results.csv'):
     
     #Combine predictions with the bools
     bool_cols  = test_df[['is_BRD4','is_HSA','is_sEH']]
@@ -261,17 +323,13 @@ def select_and_save_predictions_with_ids(predictions,test_df,path_test_file_for_
     assert len(y_pred_df)==len(test_ids)
     y_pred_and_ids_df = pd.concat([test_ids,y_pred_df],axis=1)
     
-    os.makedirs(output_dir, exist_ok=True)
-    output_path = os.path.join(output_dir,output_file_name)
+    output_path = os.path.join(results_dir,output_file_name)
     y_pred_and_ids_df.to_csv(output_path,index=False)
     return y_pred_and_ids_df
 
 def main(argv):
     print('loading training samples')    
-    #train_data = torch.load(FLAGS.train_file_path)
-    # Train model
-    #train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
-    
+
     df_train = pd.read_csv(FLAGS.train_file_csv_path)
     
     # simple featurization to get input dimension
@@ -279,35 +337,48 @@ def main(argv):
     smiles_list_train = df_train_simple['molecule_smiles'].tolist()
     labels_list = df_train_simple[['binds_BRD4','binds_HSA','binds_sEH']].values
     train_data = featurize_data_in_batches(smiles_list_train, labels_list, 64)
-
+    
+    # ---hyper-parameters
     input_dim = train_data[0].num_node_features
     hidden_dim = 64
-    num_epochs = 11
+    num_epochs = 20
     num_layers = 4 #Should ideally be set so that all nodes can communicate with each other
     dropout_rate = 0.3
     lr = 0.001
     out_channels =3
     
-    #These are just example values, feel free to play around with them.
-    #model = train_model(train_loader,num_epochs, input_dim, hidden_dim,num_layers, dropout_rate,out_channels, lr)
     device = "cuda:"+str(FLAGS.dev_num)
-    model = train_model(df_train, num_epochs, input_dim, hidden_dim, num_layers, dropout_rate,out_channels, lr,
-                featurizing_batch_size=FLAGS.featurizing_batch_size,training_batch_size=32,shard_size=FLAGS.shard_size,shuffle = True,device = device)
-    
-    #saving model
-    torch.save(model.state_dict(), FLAGS.saving_model_path)
+    # define model
+    model = GNNModel(input_dim, hidden_dim, num_layers, dropout_rate,out_channels).to(device)
 
-    print('loading test samples')    
-    test_data = torch.load(FLAGS.test_file_path)
-    test_df = pd.read_csv(FLAGS.test_csv_path)
-    # Predict
-    test_loader = DataLoader(test_data, batch_size=32, shuffle=False)
-    predictions = predict_with_model(model, test_loader,device=device)
+    os.makedirs(FLAGS.results_dir, exist_ok=True)
+
+    print('loading validation samples')    
+    #------ validation set loading
+    valid_data = torch.load(FLAGS.valid_file_path)
+    valid_loader = DataLoader(valid_data, batch_size=32, shuffle=False)
+
+    # --- Training
+    #model = train_model(train_loader,num_epochs, input_dim, hidden_dim,num_layers, dropout_rate,out_channels, lr)
+    model = train_model(df_train=df_train, model=model,valid_loader=valid_loader, lr=lr,num_epochs=num_epochs,featurizing_batch_size=FLAGS.featurizing_batch_size
+                        ,training_batch_size=32,shard_size=FLAGS.shard_size,shuffle = True,device = device,results_dir = FLAGS.results_dir)
     
-    # save predictions
     
-    _ = select_and_save_predictions_with_ids(predictions,test_df,FLAGS.test_csv_for_ids_path,output_dir = FLAGS.results_dir
-                                             ,output_file_name = FLAGS.output_file_name)
+    
+    # ----- Test set predictions ---
+    if FLAGS.test_file_path is not None:
+        print('Predicting on test samples')    
+        print('loading test samples')    
+        test_data = torch.load(FLAGS.test_file_path)
+        test_df = pd.read_csv(FLAGS.test_csv_path)
+        
+        # Predict
+        test_loader = DataLoader(test_data, batch_size=32, shuffle=False)
+        predictions = predict_with_model(model, test_loader,device=device)
+        
+        # save predictions
+        _ = select_and_save_predictions_with_ids(predictions,test_df,FLAGS.test_csv_for_ids_path,results_dir = FLAGS.results_dir
+                                                ,output_file_name = FLAGS.output_file_name)
 
         
 if __name__ == "__main__":
